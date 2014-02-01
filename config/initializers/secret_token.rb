6e3da4f45bed27c707991039ecb7ad38494691f3
Prescott Murphy <prurph@gmail.com>
2014-01-31 21:28:28 -0500

    Initial commit, add gems and update .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TubeTeam::Application.config.secret_key_base = 'ec5ee8f02a13b523b1deff55f038928e46a90686fab0dd5bc1c9bebd80baa2c292e4c7fc4ba958421e61b14d361618c2999739517105c5e6c24b2058404f4bef'
