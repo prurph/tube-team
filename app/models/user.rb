6a2866c4089c3b2ec1af928eef4bb33fd5d71d72
Prescott Murphy <prurph@gmail.com>
2014-01-31 22:04:24 -0500

    Update ApplicationController to have Devise sanitize username and enforce uniqueness thereof

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
--- db/migrate/20140201023716_devise_create_users.rb
+++ db/migrate/20140201023716_devise_create_users.rb
@@ -30,12 +30,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username, :null => false, :unique => true
+      t.string :username, :null => false
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
+    add_index :users, :username,             :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
