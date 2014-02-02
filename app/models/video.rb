4c20e0bcbb218e90d85d6be1d6e464c3e8755644
Prescott Murphy <prurph@gmail.com>
2014-02-02 10:40:27 -0500

    Refactor Video model to simplify creating videos from API; working towards returning array of videos if user searches instead of enters ID

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,45 +5,39 @@ class Video < ActiveRecord::Base
 
   validates :yt_id, presence: true, uniqueness: true
 
-  def self.make_video(youtube_id)
-
-    client = YouTubeIt::Client.new(dev_key: "AIzaSyDeEE8UySfWfxuO3hz_Qwsj4R3atx-OF70")
-    video = client.video_by(youtube_id)
-
-    api_attributes = {
-                  yt_id: video.unique_id,
-                  title: video.title,
-                  initial_watches: video.view_count,
-                  author: video.author.name,
-                  uploaded_at: video.uploaded_at,
-                  embed_html5: video.embed_html5,
-                  description: video.description,
-                  # Get the largest thumbnail available by sorting on height
-                  thumbnail: video.thumbnails.sort_by { |tn| tn.height }.last.url
-                 }
-      # Tinker with these to adjust point/watches/salaray relationships
-      custom_attributes = { salary: api_attributes[:initial_watches],
+  def self.make_attributes(video)
+    api_attr = {
+                yt_id: video.unique_id,
+                title: video.title,
+                initial_watches: video.view_count,
+                author: video.author.name,
+                uploaded_at: video.uploaded_at,
+                embed_html5: video.embed_html5,
+                description: video.description,
+                # Get the largest thumbnail available by sorting on height
+                thumbnail: video.thumbnails.sort_by { |tn| tn.height }.last.url
+                }
+    custom_attr = { salary: api_attr[:initial_watches],
                             watches: 0,
                             points: 0
-                          }
-      all_attributes = api_attributes.merge!(custom_attributes)
-      self.create!(all_attributes)
+                  }
+    return api_attr.merge!(custom_attr)
   end
 
-  # def self.new_from_yt(yt_id)
-  #   new_video = Video.create(video_params)
-  #   video_attributes = make_video(video_params[:yt_id])
+  def self.make_video(youtube_id)
+    client = YouTubeIt::Client.new(dev_key: "AIzaSyDeEE8UySfWfxuO3hz_Qwsj4R3atx-OF70")
+    video = client.video_by(youtube_id)
+    self.create!(self.make_attributes(video))
+  end
 
-  #   # Leave these here so more advanced functionality can be set later
-  #   # For now salary is equal to watches at time video info is pulled
-  #   video_attributes.merge!({ salary: video_attributes[:initial_watches],
-  #                             watches: 0,
-  #                             points: 0 })
-  #   video = Video.create(video_attributes)
-
-  #   # Video exists now, so pass to videos#edit with team info so user can sign
-  #   redirect_to video
-  # end
+  def self.search_videos(search_term)
+    client = YouTubeIt::Client.new(dev_key: "AIzaSyDeEE8UySfWfxuO3hz_Qwsj4R3atx-OF70")
+    # Run the search and return top 5 results
+    videos = client.videos_by(query: search_term).videos.slice!(0,5)
+    # Make an array of video objects from the API results
+    videos.map!{|video| Video.make_video(video.unique_id)}
+    return videos
+  end
 
   def update_points(start_time=Time.new(1982), end_time=Time.now)
     updates = WatchUpdate.all(conditions:
