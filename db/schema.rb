cca22f151b780f18eca06c667887b55ad00ab6c6
Prescott Murphy <prurph@gmail.com>
2014-02-02 20:37:25 -0500

    Add timestamps to teams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202033518) do
+ActiveRecord::Schema.define(version: 20140203013627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "teams", force: true do |t|
-    t.integer "salary"
-    t.text    "name"
-    t.integer "watches",  default: 0
-    t.integer "user_id"
-    t.integer "points",   default: 0
-    t.integer "bankroll"
+    t.integer  "salary"
+    t.text     "name"
+    t.integer  "watches",    default: 0
+    t.integer  "user_id"
+    t.integer  "points",     default: 0
+    t.integer  "bankroll"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "teams", ["user_id"], name: "index_teams_on_user_id", using: :btree
